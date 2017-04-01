723efbbc9d51defd3f7e91f098ac55116aa898f0
chenmingyang <mingyang2013@qq.com>
2017-04-01 23:57:06 +0800

    初始化项目结构，引入maven依赖等。

---
+++ src/test/java/me/mingyang/blog/DemoTest.java
@@ -0,0 +1,14 @@
+package me.mingyang.blog;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DemoTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
